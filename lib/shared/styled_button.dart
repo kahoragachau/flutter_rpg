4f7a4da5f5f3280df9381d67ae1a59a704a99f48
kahoragachau <joelkahoragachau6@gmail.com>
2024-03-28 23:09:13 +0300

    style: Add gradient color to the button

--- lib/shared/styled_button.dart
+++ lib/shared/styled_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/theme.dart';
 
 class StyledButton extends StatelessWidget {
   const StyledButton({
@@ -16,8 +17,16 @@ class StyledButton extends StatelessWidget {
       onPressed: onPressed,
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [AppColors.primaryColor, AppColors.primaryAccent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(5))
+        ),
         child: child,
-      ),
+      )
     );
   }
 }
