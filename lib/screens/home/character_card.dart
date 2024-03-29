11fefc2547edbb0387ca7321e096b08c18beb636
kahoragachau <joelkahoragachau6@gmail.com>
2024-03-29 23:03:18 +0300

    feat: Create a Character Card Widget

--- lib/screens/home/character_card.dart
+++ lib/screens/home/character_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CharacterCard extends StatelessWidget {
-  const CharacterCard({super.key});
+  const CharacterCard(this.character,{super.key});
+
+  final String character;
 
   @override
   Widget build(BuildContext context) {
