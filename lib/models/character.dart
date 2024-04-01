d347f476fd06edbf9b56fff75d4d5fdc80edbe51
kahoragachau <joelkahoragachau6@gmail.com>
2024-04-01 16:20:40 +0300

    feat: Create Character Model

---
+++ lib/models/character.dart
@@ -0,0 +1,9 @@
+class Character {
+
+  // fields: note that keyword 'final' basically means the value won't change in the future, 
+  //         the '_' is used to represent a private field
+  final String name;
+  final String slogan;
+  final String id;
+  bool _isFav = false;
+}
