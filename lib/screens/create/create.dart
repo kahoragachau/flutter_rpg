3dd4dbe8eed26bb1c3fa8308225c0315d954f445
kahoragachau <joelkahoragachau6@gmail.com>
2024-04-22 13:57:31 +0300

    feat: Add title and heading

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
 
 class Create extends StatefulWidget {
   const Create({super.key});
@@ -10,6 +12,29 @@ class Create extends StatefulWidget {
 class _CreateState extends State<Create> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const StyledTitle("Characters Creation"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        child: Column(
+          children: [
+            // welcome message
+            Center(
+              child: Icon(Icons.code, color: AppColors.primaryColor)
+            ),
+            const Center(
+              child: StyledHeading("Welcome New Player"),
+            ),
+            const Center(
+              child: StyledText("Create a name & slogan for your character."),
+            ),
+            const SizedBox(height: 30),
+          ],
+        ),
+      ),
+    );
   }
 }
