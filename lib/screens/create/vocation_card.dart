8c9da9ddda0a1f0e2c3b685c5bdeaa91b56cad4c
kahoragachau <joelkahoragachau6@gmail.com>
2024-04-26 15:02:52 +0300

    feat: Create Vocation Card

---
+++ lib/screens/create/vocation_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VocationCard extends StatelessWidget {
+  const VocationCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
