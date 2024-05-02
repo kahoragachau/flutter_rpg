0d0462699be06a699560dd04d055399fe3a23032
kahoragachau <joelkahoragachau6@gmail.com>
2024-05-02 14:55:12 +0300

    feat: Add Navigation from Character Screen to Profile Screen

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const StyledTitle('Character Name'),
+      ),
+
+      body: const SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+          ],
+        ),
+      ),
+    );
   }
 }
