bb636cf8fd54db5461b905d7c826122c01ca4988
kahoragachau <joelkahoragachau6@gmail.com>
2024-05-06 23:14:45 +0300

    feat: Create Stats Table Module

---
+++ lib/screens/profile/stats_table.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StatsTable extends StatefulWidget {
+  const StatsTable({super.key});
+
+  @override
+  State<StatsTable> createState() => _StatsTableState();
+}
+
+class _StatsTableState extends State<StatsTable> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
