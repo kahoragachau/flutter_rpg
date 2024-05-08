239b804544309d9f158e376b5e2a6b0517d6ac61
kahoragachau <joelkahoragachau6@gmail.com>
2024-05-08 12:11:46 +0300

    feat: Add Snackbar

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/screens/profile/skill_list.dart';
 import 'package:flutter_rpg/screens/profile/stats_table.dart';
+import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 
@@ -86,7 +87,19 @@ class Profile extends StatelessWidget {
                   SkillList(character),
                 ],
               ),
-            )
+            ),
+
+            // Save Button
+            StyledButton(onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const StyledHeading('Charcter was saved.'),
+                showCloseIcon: true,
+                duration: const Duration(seconds: 2),
+                backgroundColor: AppColors.secondaryColor,
+              ));
+            }, 
+            child: const StyledHeading('Save Character')),
+            const SizedBox(height: 20),
           ],
         ),
       ),
