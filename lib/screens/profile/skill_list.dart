54308a49f11cd6834980ffeb76d976c29a1ec97f
kahoragachau <joelkahoragachau6@gmail.com>
2024-05-08 10:50:55 +0300

    feat: Output Vocation Skills

--- lib/screens/profile/skill_list.dart
+++ lib/screens/profile/skill_list.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/models/skill.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
 
 class SkillList extends StatefulWidget {
   const SkillList( this.character, {super.key});
@@ -11,8 +14,42 @@ class SkillList extends StatefulWidget {
 }
 
 class _SkillListState extends State<SkillList> {
+  // will be available later thats why we us late
+  late List<Skill> availableSkill;
+
+  @override
+  void initState() {
+    availableSkill = allSkills.where((skill) {
+      return skill.vocation == widget.character.vocation;
+    }).toList();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        color: AppColors.secondaryColor.withOpacity(0.5),
+        child: Column(
+          children: [
+            const StyledHeading('Choose an Active Skill'),
+            const StyledText('Skills are unique to your vocation'),
+            const SizedBox(height: 20,),
+
+            Row(
+              children: availableSkill.map((skill) {
+                return Container(
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(2),
+                  child: Image.asset('assets/img/skills/${skill.image}', width: 70,),
+                )
+              }).toList();
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
