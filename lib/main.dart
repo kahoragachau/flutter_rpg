b28bac1fd5e7791206a0583782056fb70ba69b49
kahoragachau <joelkahoragachau6@gmail.com>
2024-05-09 21:47:54 +0300

    feat: wrap main module with changeprovider notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/services/character_store.dart';
 import 'package:flutter_rpg/theme.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
+    create: (context) => CharacterStore(),
     child: MaterialApp(
       theme: primaryTheme,
       home: const Home(),
