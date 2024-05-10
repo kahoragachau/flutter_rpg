892b492c99ba61fa48ff9799e51bf11953773ab4
kahoragachau <joelkahoragachau6@gmail.com>
2024-05-10 12:06:34 +0300

    feat: Setup Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   // initialize firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
