ef980a8ff33a432e1ebbe75a67c3597d9da09fa4
kahoragachau <joelkahoragachau6@gmail.com>
2024-05-25 11:39:47 +0300

    style: Add the heart Icon to profile info-section

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/screens/profile/heart.dart';
 import 'package:flutter_rpg/screens/profile/skill_list.dart';
 import 'package:flutter_rpg/screens/profile/stats_table.dart';
 import 'package:flutter_rpg/services/character_store.dart';
@@ -29,30 +31,39 @@ class Profile extends StatelessWidget {
           children: [
 
             // Basic Info 
-            Container(
-              padding: const EdgeInsets.all(16),
-              color: AppColors.secondaryColor.withOpacity(0.3),
-              child: Row(
-                children: [
-                  Hero(
-                    tag: character.id.toString(),
-                    child: Image.asset('assets/img/vocations/${character.vocation.image}',
-                    width: 140,
-                    height: 140,
-                    ),
-                  ),
-                const SizedBox(width: 30,),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+            Stack(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  color: AppColors.secondaryColor.withOpacity(0.3),
+                  child: Row(
                     children: [
-                      StyledHeading(character.vocation.title),
-                      StyledText(character.vocation.description),
+                      Hero(
+                        tag: character.id.toString(),
+                        child: Image.asset('assets/img/vocations/${character.vocation.image}',
+                        width: 140,
+                        height: 140,
+                        ),
+                      ),
+                    const SizedBox(width: 30,),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          StyledHeading(character.vocation.title),
+                          StyledText(character.vocation.description),
+                        ],
+                      ),
+                    )
                     ],
                   ),
-                )
-                ],
-              ),
+                ),
+                Positioned(
+                  top: 10,
+                  right: 10,
+                  child: Heart(character: character)
+                ),
+              ],
             ),
 
             // Weapon And ability And Slogan 
